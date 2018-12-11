0b3f1befe8737063285f2f1abf15842c2f5437ca
Zachary Murphy <ztmurphy21@gmail.com>
2018-12-10 21:59:04 -0600

    update faq.js file
    additional comments/documentation

--- frequently_asked_questions/faqs.js
+++ frequently_asked_questions/faqs.js
@@ -3,14 +3,22 @@ FAQS App JS file
 Author: Zachary Murphy
 12-10-2018*/
 $(document).ready(function(){
-    $("#faqs h2").toggle(
-        function(){
+    $("#faqs h2").toggle( //toggle method
+        function(){ //toggle method function 1
+
+            //add a class attribute to the h2 element with a minus
             $(this).addClass("minus");
+            //chain next and show methods to display hiddne div element content
             $(this).next().show();
+
         },
-        function(){
+        function(){ //togle method function 2 
+                    //this function works reverse of the first one
+                    
+            //remove minus class 
             $(this).removeClass("minus");
+            //use of next and hide methods per div element
             $(this).next().hide();
         }
-    ); //end of toggle 
-}); //end ready
+    ); //end of toggle method
+}); //end ready function
